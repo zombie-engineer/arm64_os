94f34b23253408fa83736e9d2af6f9ef36356525
Val <valery.vdovin.s@gmail.com>
2019-08-21 20:12:09 +0300

    Work on MMU

--- mmu.c
+++ mmu.c
@@ -1,30 +1,6 @@
-/*
- * Copyright (C) 2018 bzt (bztsrc@github)
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use, copy,
- * modify, merge, publish, distribute, sublicense, and/or sell copies
- * of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- *
- */
-
-#include "gpio.h"             // get MMIO_BASE
+#include "gpio.h"
 #include "uart.h"
+#include "common.h"
 
 #define PAGESIZE    4096
 
@@ -59,6 +35,7 @@ void mmu_init()
 {
     unsigned long data_page = (unsigned long)&_data/PAGESIZE;
     unsigned long r, b, *paging=(unsigned long*)&_end;
+    printf("data page: %d, paging at 0x%016lx\n", (int)data_page, paging);
 
     /* create MMU translation tables at _end */
 
