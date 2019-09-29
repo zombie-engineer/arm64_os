290773b7857fe2bb6ca61e80a00d63c64bff6f54
Val <valery.vdovin.s@gmail.com>
2019-09-29 17:20:09 +0300

    Added tabs to console output

---
+++ include/console_char.h
@@ -0,0 +1,17 @@
+#pragma once
+
+/* backspace */
+#define CONSOLE_CHAR_BACKSPACE       0x08
+
+/* horizontal tab */
+#define CONSOLE_CHAR_HORIZONTAL_TAB  0x09
+
+/* newline '\n' */
+#define CONSOLE_CHAR_LINEFEED        0x0a
+
+/* carriage return '\r' */
+#define CONSOLE_CHAR_CARRIAGE_RETURN 0x0d
+
+/* delete char */
+#define CONSOLE_CHAR_DEL             0x7f
+
--- lfb.c
+++ lfb.c
@@ -1,13 +1,15 @@
 #include <lfb.h>
 #include <uart/uart.h>
 #include <mbox/mbox.h>
+#include <console_char.h>
 #include "homer.h"
 #include "exception.h"
 
 unsigned int fb_width, fb_height, fb_pitch;
 unsigned char *lfb;
 static int lfb_initialized = 0;
-static int lfb_bgcolor;
+static int lfb_bgcolor = 0;
+static int lfb_tabwidth = 1;
 
 typedef struct {
   unsigned int magic;
@@ -86,84 +88,9 @@ void lfb_init(int width, int height)
   {
     uart_puts("Unable to set screen resolution to 1024x768x32\n");
   }
+
+  lfb_tabwidth = 4;
   lfb_initialized = 1;
-}
-
-void lfb_set_bgcolor(int value)
-{
-  lfb_bgcolor = value;
-}
-
-void lfb_showpicture()
-{
-  int x,y;
-  unsigned char *ptr = lfb;
-  char *data = homer_data, pixel[4];
-  ptr += (fb_height - homer_height) / 2 * fb_pitch + (fb_width - homer_width) * 2;
-  for (y = 0; y < homer_height; ++y)
-  {
-    for (x = 0; x < homer_width; ++x)
-    {
-      HEADER_PIXEL(data, pixel);
-      *((unsigned int*)ptr) = *((unsigned int*)&pixel);
-      ptr += 4;
-    }
-    ptr += fb_pitch - homer_width * 4;
-  }
-}
-
-void lfb_print(int x, int y, char *s)
-{
-  psf_t *font = (psf_t*)&_binary_font_psf_start;
-  while(*s) {
-    // get offset to the glyph. Need to adjust this to support unicode..
-    unsigned char *glyph = (unsigned char*)&_binary_font_psf_start + font->headersize
-      + (*((unsigned char*)s) < font->numglyph ? *s : 0) * font->bytesperglyph;
-    // calculate offset on screen
-    int offs = (y * font->height * fb_pitch) + (x * (font->width + 1) * 4);
-    
-    int i, j, line, mask, bytesperline = (font->width + 7) / 8;
-    // handle carriage return
-    if (*s == '\r') {
-      x = 0;
-    } else
-    // new line
-    if (*s == '\n') {
-      x = 0; y++;
-    } else {
-      // display character
-      for (j = 0; j < font->height; ++j) {
-        line = offs;
-        mask = 1 << (font->width - 1);
-        for (i = 0; i < font->width; ++i) {
-          *((unsigned int*)(lfb + line)) = ((int)*glyph) & mask ? 0xffffff : 0;
-          mask >>= 1;
-          line += 4;
-        }
-        // adjust to next line
-        glyph += bytesperline;
-        offs += fb_pitch;
-      }
-      x++;
-    }
-    // next character
-    s++;
-  }
-}
-
-void lfb_print_long_hex(int x, int y, unsigned long val)
-{
-  char* dump_place = (char*)0x90000;
-  char* ptr = dump_place;
-  unsigned long n;
-  int c;
-  for(c = 60; c >= 0; c -=4) {
-    n = (val >> c) & 0xf;
-    n += n > 9 ? 0x37 : 0x30;
-    *ptr++ = n;
-  }
-  *ptr = 0;
-  lfb_print(x, y, dump_place);
 }
 
 static void fill_background(psf_t *font, char *framebuf_off, int pitch)
@@ -197,6 +124,32 @@ static void print_glyph(unsigned char* glyph_off, psf_t *font, char *framebuf_of
   }
 }
 
+
+void lfb_set_bgcolor(int value)
+{
+  lfb_bgcolor = value;
+}
+
+
+void lfb_showpicture()
+{
+  int x,y;
+  unsigned char *ptr = lfb;
+  char *data = homer_data, pixel[4];
+  ptr += (fb_height - homer_height) / 2 * fb_pitch + (fb_width - homer_width) * 2;
+  for (y = 0; y < homer_height; ++y)
+  {
+    for (x = 0; x < homer_width; ++x)
+    {
+      HEADER_PIXEL(data, pixel);
+      *((unsigned int*)ptr) = *((unsigned int*)&pixel);
+      ptr += 4;
+    }
+    ptr += fb_pitch - homer_width * 4;
+  }
+}
+
+
 void lfb_putc(int* x, int* y, char chr)
 {
   unsigned char *glyphs, *glyph;
@@ -213,22 +166,28 @@ void lfb_putc(int* x, int* y, char chr)
   // calculate offset on screen
   framebuf_off = (*y * font->height * fb_pitch) + (*x * (font->width + 1) * 4);
   
-  if (chr == '\r') {
-    // carriage return
-    *x = 0;
-  } else if (chr == '\n') {
-    // new line
-    *x = 0; 
-    (*y)++;
-  } else if (chr == 8 || chr == 127) {
-    // backspace
-    if (*x > 0)
-      (*x)--;
-    framebuf_off = (*y * font->height * fb_pitch) + (*x * (font->width + 1) * 4);
-    fill_background(font, ((char*)lfb) + framebuf_off, fb_pitch);
-  } else {
-    print_glyph(glyph, font, ((char*)lfb) + framebuf_off, fb_pitch);
-    (*x)++;
+  switch(chr) {
+    case CONSOLE_CHAR_CARRIAGE_RETURN:
+      *x = 0;
+      break;
+    case CONSOLE_CHAR_LINEFEED:
+      *x = 0; 
+      (*y)++;
+      break;
+    case CONSOLE_CHAR_HORIZONTAL_TAB:
+      *x += lfb_tabwidth;
+      break;
+    case CONSOLE_CHAR_BACKSPACE:
+    case CONSOLE_CHAR_DEL:
+      if (*x > 0)
+        (*x)--;
+      framebuf_off = (*y * font->height * fb_pitch) + (*x * (font->width + 1) * 4);
+      fill_background(font, ((char*)lfb) + framebuf_off, fb_pitch);
+      break;
+    default:
+      print_glyph(glyph, font, ((char*)lfb) + framebuf_off, fb_pitch);
+      (*x)++;
+      break;
   }
 }
 
