c5e0c9931050152a3a2f46a73b4a3a9e968d1658
Val <valery.vdovin.s@gmail.com>
2019-12-29 20:03:08 +0300

    Improved exception handling asm code

--- include/exception.h
+++ include/exception.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <compiler.h>
+#include <types.h>
+
 typedef int(*irq_cb_t)();
 
 void set_irq_cb(irq_cb_t cb);
@@ -9,3 +12,13 @@ void set_fiq_cb(irq_cb_t cb);
 void generate_exception();
 
 void __handle_interrupt();
+
+typedef struct exception_info {
+  uint64_t esr; 
+  uint64_t spsr; 
+  uint64_t elr; 
+  uint64_t far;
+  uint64_t type;
+  void *cpu_ctx;
+  uint64_t *stack;
+} packed exception_info_t;
