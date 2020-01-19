b7066f38bda846a92e9de15e107f50b74d4bcf33
Val <valery.vdovin.s@gmail.com>
2020-01-19 23:06:59 +0300

    spinlock_init

--- include/spinlock.h
+++ include/spinlock.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#define spinlock_init __armv8_spinlock_init
 #define spinlock_lock __armv8_spinlock_lock
 #define spinlock_unlock __armv8_spinlock_unlock
 
+extern void __armv8_spinlock_init(void *spinlock);
 extern void __armv8_spinlock_lock(void *spinlock);
 extern void __armv8_spinlock_unlock(void *spinlock);
