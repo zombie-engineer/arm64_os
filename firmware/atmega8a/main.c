bc0fd30ba731c2d9944345f5e1f04fa9a3bc149f
Val <antiusninja@gmail.com>
2020-03-24 21:36:45 +0300

    Improved delay/wait functions in atmega code

--- firmware/atmega8a/main.c
+++ firmware/atmega8a/main.c
@@ -1,5 +1,15 @@
 #include "atmega8.h"
-extern void delay_cycles(short num_cycles);
+
+typedef unsigned short uint16_t;
+typedef short int16_t;
+typedef unsigned int uint32_t;
+typedef int int32_t;
+
+extern void delay_cycles_16(uint16_t num_cycles);
+extern void countdown_32(uint16_t count_hi, uint16_t count_low);
+#define LOW16(v) (v & 0xffff)
+#define HIGH16(v) ((v >> 16) & 0xffff)
+#define ARG_SPLIT_32(v) HIGH16(v), LOW16(v)
 
 #define PIN_MODE_OUT(port, pin) \
   asm volatile("sbi %0, %1\n"::"i"(DDR ## port), "i"(pin))
@@ -13,20 +23,26 @@ extern void delay_cycles(short num_cycles);
 #define PIN_OFF(port, pin) \
   asm volatile("cbi %0, %1\n"::"i"(PORT ## port), "i"(pin))
 
-
-void blink() 
+void __attribute__((optimize("O2"))) blink() 
 {
-  PIN_ON(B, 0);
-  delay_cycles(1000);
-  PIN_OFF(B, 0);
-  delay_cycles(1000);
+  char *blinker = 0x100;
+  if (*blinker == 1) {
+    *blinker = 0;
+    PIN_ON(B, 0);
+  } else {
+    *blinker = 1;
+    PIN_OFF(B, 0);
+  }
+//  delay_cycles_16(1000);
+//  delay_cycles_16(1000);
 }
 
 void main() 
 {
+  *(char*)0x100 = 0;
   PIN_MODE_OUT(B, 0);
   while(1) {
-    delay_cycles(65000);
+    countdown_32(ARG_SPLIT_32(200000));
     blink();
   }
 }
