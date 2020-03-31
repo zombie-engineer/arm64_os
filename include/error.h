966880947667dec1f872c5fde3ea5572346f41d9
Val <antiusninja@gmail.com>
2020-03-31 10:55:18 +0300

    Implemented linux kernel based PTR_ERR

--- include/error.h
+++ include/error.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <types.h>
 
 #define ERR_OK               0
 #define ERR_INVAL_ARG       -1
@@ -9,3 +10,19 @@
 #define ERR_NO_RESOURCE     -6
 #define ERR_NOT_FOUND       -7
 #define ERR_BUSY            -8
+#define MAX_ERRNO          128
+
+static inline void *ERR_PTR(int64_t err) 
+{
+  return (void *)err;
+}
+
+static inline int IS_ERR(const void *ptr)
+{
+  return (uint64_t)ptr >= (uint64_t)(-MAX_ERRNO);
+}
+
+static inline int64_t PTR_ERR(const void *ptr)
+{
+  return (int64_t)ptr;
+}
