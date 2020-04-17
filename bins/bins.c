666eec48c23bdb11776d770c9ae1048986a474a0
Val <valery.vdovin.s@gmail.com>
2020-04-17 08:12:25 +0300

    Put bins into header

--- bins/bins.c
+++ bins/bins.c
@@ -1,5 +1,2 @@
 #include <bins.h>
 
-DECL_BIN_OBJECTS(atmega)
-DECL_BIN_OBJECTS(nokia5110_animation)
-DECL_BIN_OBJECTS(font)
--- include/bins.h
+++ include/bins.h
@@ -7,11 +7,15 @@
 #define DECL_BIN_OBJECTS(x) \
   extern char BIN_START(x);\
   extern char BIN_END(x);\
-  void *bins_get_start_ ## x() \
+  static inline void *bins_get_start_ ## x() \
   { \
     return &BIN_START(x);\
   } \
-  int bins_get_size_ ## x()\
+  static inline int bins_get_size_ ## x()\
   { \
     return BIN_SIZE(x);\
   }
+
+DECL_BIN_OBJECTS(atmega)
+DECL_BIN_OBJECTS(nokia5110_animation)
+DECL_BIN_OBJECTS(font)
