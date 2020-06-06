ce810ba7ecc95ff494e797cfcdb331dd7e548dee
Val <antiusninja@gmail.com>
2020-06-06 19:00:21 +0300

    Added percpu_data variable

---
+++ include/arch/armv8/percpu_asm_macros.h
@@ -0,0 +1,96 @@
+/*
+ * Macros below rely heavily on cache line width, assuming it is 64 bytes.
+ * Currently I don't have any good ideas of how this could be solved.
+ */
+
+.equ CACHE_LINE_WIDTH_LOG, 6
+.equ CACHE_LINE_WIDTH, 64
+.macro PERCPU_DATA
+.align CACHE_LINE_WIDTH_LOG
+GLOBAL_VAR(__percpu_data):
+.rept NUM_CORES
+/*
+ * Stack pointer EL0
+ */
+.align CACHE_LINE_WIDTH_LOG
+.quad 0
+
+/*
+ * Stack pointer EL1
+ */
+.quad 0
+
+/*
+ * Jump instruction address
+ */
+.quad 0
+
+/*
+ * Value of MPIDR_EL1 register
+ */
+.quad 0
+
+/*
+ * Address to saved CPU state
+ */
+.quad 0
+
+/*
+ * Padding to 64 bytes ( cache line width )
+ */
+// .rept (CACHE_LINE_WIDTH / 8 - 5)
+// .quad 0
+// .endr /* 64 bytes padding */
+.endr /* NUM_CORES */
+.endm
+
+.equ PERCPU_OFFSET_STACK_EL0    , 0
+.equ PERCPU_OFFSET_STACK_EL1    , 8
+.equ PERCPU_OFFSET_JUMP_ADDR    , 16
+.equ PERCPU_OFFSET_MPIDR_EL1    , 24
+.equ PERCPU_OFFSET_CPU_STATE    , 32
+
+.macro get_percpu_data out, cpu_num
+  ldr \out, =__percpu_data
+  add \out, \out, \cpu_num, lsl #CACHE_LINE_WIDTH_LOG
+.endm
+
+.macro percpu_data_set_stack cpu, el, val, tmp
+   get_percpu_data \tmp, \cpu
+   str \val, [\tmp, PERCPU_OFFSET_STACK_EL(\el)]
+.endm
+
+.macro percpu_data_get_stack cpu, el, out, tmp
+   get_percpu_data \tmp, \cpu
+   ldr \out, [\tmp, PERCPU_OFFSET_STACK_EL(\el)]
+.endm
+
+.macro percpu_data_set_jmp cpu, val, tmp
+   get_percpu_data \tmp, \cpu
+   str \val, [\tmp, PERCPU_OFFSET_JUMP_ADDR]
+.endm
+
+.macro percpu_data_get_jmp cpu, out, tmp
+   get_percpu_data \tmp, \cpu
+   ldr \out, [\tmp, PERCPU_OFFSET_JUMP_ADDR]
+.endm
+
+.macro set_percpu_mpidr cpu, val, tmp
+   get_percpu_data \tmp, \cpu_num
+   str \val, [\tmp, PERCPU_OFFSET_MPIDR_EL1]
+.endm
+
+.macro get_percpu_mpidr cpu, out, tmp
+   get_percpu_data \tmp, \cpu_num
+   ldr \out, [\tmp, PERCPU_OFFSET_MPIDR_EL1]
+.endm
+
+.macro set_percpu_cpu_state cpu, val, tmp
+   get_percpu_data \tmp, \cpu_num
+   str \val, [\tmp, PERCPU_OFFSET_CPU_STATE]
+.endm
+
+.macro get_percpu_cpu_state cpu, out, tmp
+   get_percpu_data \tmp, \cpu_num
+   ldr \out, [\tmp, PERCPU_OFFSET_CPU_STATE]
+.endm
