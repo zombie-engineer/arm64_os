d84c72c6e1a9e6f435572186b9952eab651beb12
Val <antiusninja@gmail.com>
2020-07-05 10:33:20 +0300

    Added uart console lock

--- uart/uart.c
+++ uart/uart.c
@@ -3,6 +3,7 @@
 #include <uart/pl011_uart.h>
 #include <uart/mini_uart.h>
 #include <uart/uart.h>
+#include <spinlock.h>
 
 #include <error.h>
 
@@ -11,14 +12,14 @@
 #endif
 
 #if defined(CONFIG_UART_PL011)
-#define uart_send pl011_uart_send 
+#define uart_send pl011_uart_send
 #define uart_recv pl011_uart_getc
 #define _uart_init pl011_uart_init
 #define _uart_send_buf pl011_uart_send_buf
 #define _uart_set_interrupt_mode pl011_uart_set_interrupt_mode
 #define _uart_subscribe_to_rx_event pl011_uart_subscribe_to_rx_event
 #elif defined(CONFIG_UART_MINI)
-#define uart_send mini_uart_send 
+#define uart_send mini_uart_send
 #define uart_recv mini_uart_getc
 #define _uart_init mini_uart_init
 #define _uart_send_buf mini_uart_send_buf
@@ -26,9 +27,11 @@
 #define _uart_subscribe_to_rx_event mini_uart_subscribe_to_rx_event
 #endif
 
+static DECL_SPINLOCK(uart_lock);
 void uart_init(int baudrate, int system_clock)
 {
   _uart_init(baudrate, system_clock);
+  spinlock_init(&uart_lock);
 }
 
 int uart_putc(char c)
@@ -40,17 +43,38 @@ int uart_putc(char c)
 char uart_getc()
 {
   char r;
+  bool locked = false;
+  if (spinlocks_enabled) {
+    spinlock_lock(&uart_lock);
+    locked = true;
+  }
   r = uart_recv();
+
+  if (locked) {
+    spinlock_unlock(&uart_lock);
+    locked = false;
+  }
   return r == '\r' ? '\n' : r;
 }
 
 int uart_puts(const char *s)
 {
+  bool locked = false;
+  if (spinlocks_enabled) {
+    spinlock_lock(&uart_lock);
+    locked = true;
+  }
+
   while(*s)
   {
     if (*s == '\n')
       uart_send('\r');
     uart_send(*s++);
+  }
+
+  if (locked) {
+    spinlock_unlock(&uart_lock);
+    locked = false;
   }
   return ERR_OK;
 }
