4d4028c4f339e03149935379c06bf8739ab71f42
Val <antiusninja@gmail.com>
2020-07-16 01:33:09 +0300

    Added topo_addr for usb devices. Extracted generic part of device printers

--- drivers/usbd/usbd.c
+++ drivers/usbd/usbd.c
@@ -3,8 +3,9 @@
 #include <drivers/usb/hcd_hub.h>
 #include <error.h>
 #include <common.h>
+#include <sched.h>
 
-int usbd_init()
+int usbd_init(void)
 {
   int err;
   err = usb_hcd_init();
@@ -14,21 +15,16 @@ int usbd_init()
   return err;
 }
 
-static void usbd_print_device_recursive(struct usb_hcd_device *d, int depth)
+static void usbd_print_device_summary(const char *padding, const char *subpadding, const struct usb_topo_addr *ta, struct usb_hcd_device *d)
 {
-  usb_hub_t *h;
-  int i;
-  struct usb_hcd_device *child;
-  char padding[128];
-  char *subpadding = "  ";
-  prefix_padding_to_string(" ", ' ', depth, 2, padding, sizeof(padding)); 
-
-  printf("%sport:%02d: USB device: addr=%02d id=%04x:%04x class=%s(%d)" __endline,
+  char topoaddr[32];
+  usb_topo_addr_to_string(topoaddr, sizeof(topoaddr), ta);
+  printf("%s%s: addr:%02d id:%04x:%04x class:%s(%d)" __endline,
     padding,
-    d->location.hub_port,
+    topoaddr,
     d->address,
     d->descriptor.id_vendor,
-    d->descriptor.id_product, 
+    d->descriptor.id_product,
     d->class ? usb_hcd_device_class_to_string(d->class->device_class) : "NONE",
     d->class ? d->class->device_class : 0);
 
@@ -36,6 +32,19 @@ static void usbd_print_device_recursive(struct usb_hcd_device *d, int depth)
     padding,
     subpadding,
     d->string_manufacturer, d->string_product, d->string_serial);
+}
+
+static void usbd_print_device_recursive(struct usb_hcd_device *d, int depth)
+{
+  usb_hub_t *h;
+  int i;
+  struct usb_topo_addr ta;
+  struct usb_hcd_device *child;
+  char padding[128];
+  char *subpadding = "  ";
+  prefix_padding_to_string(" ", ' ', depth, 2, padding, sizeof(padding));
+  usb_hcd_get_topo_addr(d, &ta);
+  usbd_print_device_summary(padding, subpadding, &ta, d);
 
   for (i = 0; i < d->num_interfaces; ++i) {
     struct usb_hcd_interface *iface = &d->interfaces[i];
@@ -49,7 +58,7 @@ static void usbd_print_device_recursive(struct usb_hcd_device *d, int depth)
     switch(d->class->device_class) {
       case USB_HCD_DEVICE_CLASS_HUB:
         h = usb_hcd_device_to_hub(d);
-        printf("%s%snum_ports=%d" __endline, padding, 
+        printf("%s%snum_ports=%d" __endline, padding,
           subpadding,
           h->descriptor.port_count);
         for (i = 0; i < h->descriptor.port_count; ++i) {
@@ -65,7 +74,85 @@ static void usbd_print_device_recursive(struct usb_hcd_device *d, int depth)
   puts(__endline);
 }
 
-void usbd_print_device_tree()
+void usbd_print_device_tree(void)
 {
   usbd_print_device_recursive(root_hub, 0 /* recursive depth */);
 }
+
+void usbd_monitor(void)
+{
+  struct usb_hcd_device *d;
+  struct usb_topo_addr ta = {
+    .ports = { 0, 0 },
+    .depth = 2
+  };
+  d = usbd_find_device_by_topo_addr(&ta);
+  if (!d) {
+    printf("failed to find d\n");
+    while(1);
+  }
+  usbd_print_device_summary("-", "--", &ta, d);
+
+  while(1) {
+    wait_on_timer_ms(1000);
+    // putc('-');
+    usb_hcd_print_intr_regs();
+  }
+}
+
+void usb_hcd_get_topo_addr(const struct usb_hcd_device *d, struct usb_topo_addr *ta)
+{
+  int i = -1;
+  const struct usb_hcd_device *dtmp = d;
+  char ports[ARRAY_SIZE(ta->ports)];
+  // printf("usb_hcd_get_topo_addr\n");
+  while(dtmp) {
+    // printf("dtmp:%p, parent:%p\n", dtmp, dtmp->location.hub);
+    ports[++i] = dtmp->location.hub_port;
+    dtmp = dtmp->location.hub;
+  }
+  ta->depth = 0;
+  while(i + 1) {
+    // printf("ta->ports:%d\n", i);
+    ta->ports[ta->depth++] = ports[i--];
+  }
+}
+
+struct usb_hcd_device *usbd_find_device_by_topo_addr(const struct usb_topo_addr *addr)
+{
+  int i;
+  usb_hub_t *hub;
+  struct usb_hcd_device *d = root_hub;
+  printf("root_hub: %p, class: %p\n", d, d->class);
+
+  for (i = 0; i < addr->depth; ++i) {
+    puts("-");
+    if (!d->class || d->class->device_class != USB_HCD_DEVICE_CLASS_HUB) {
+      puts("!");
+      return NULL;
+    }
+    hub = usb_hcd_device_to_hub(d);
+    d = usb_hub_get_device_at_port(hub, addr->ports[i]);
+    if (!d) {
+      puts("<");
+      return NULL;
+    }
+  }
+  return d;
+}
+
+int usb_topo_addr_to_string(char *buf, int bufsz, const struct usb_topo_addr *ta)
+{
+  int n = 0;
+  int i;
+  // printf("usb_topo_addr_to_string: ta:depth:%d\n", ta->depth);
+  for (i = 0; i + 1 < ta->depth; ++i) {
+    n += snprintf(buf + n, bufsz - n, "%02d.", ta->ports[i]);
+    // printf("usb_topo_addr_to_string: ta:depth:%d-\n", ta->depth);
+  }
+
+  n += snprintf(buf + n, bufsz - n, "%02d", ta->ports[i]);
+  // printf("usb_topo_addr_to_string: ta:depth:%d--\n", ta->depth);
+  return n;
+}
+
--- include/drivers/usb/usbd.h
+++ include/drivers/usb/usbd.h
@@ -1,5 +1,35 @@
 #pragma once
 
-int usbd_init();
+int usbd_init(void);
 
-void usbd_print_device_tree();
+void usbd_print_device_tree(void);
+
+void usbd_monitor(void);
+
+struct usb_hcd_device;
+
+/*
+ * Topological or hierarchial address of a device, that enables search,
+ * by specifying recursive port numbers.
+ * USB hierarchy is backed by a physical composition of hubs connected to
+ * parent hub ports, so it is possible to address to a specific device by
+ * it's address starting from the port number on the root hub and walking
+ * down the series of other hub ports until the required device is attached.
+ */
+struct usb_topo_addr {
+  /*
+   * array of port numbers starting from root hub port.
+   */
+  char ports[16];
+
+  /*
+   * actual number of valid entries in ports array until the end device.
+   */
+  int depth;
+};
+
+void usb_hcd_get_topo_addr(const struct usb_hcd_device *d, struct usb_topo_addr *ta);
+
+int usb_topo_addr_to_string(char *buf, int bufsz, const struct usb_topo_addr *ta);
+
+struct usb_hcd_device *usbd_find_device_by_topo_addr(const struct usb_topo_addr *ta);
