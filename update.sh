41d3f7d57f8ac6ca2815cbd76e1b259a2fd39f64
Val <antiusninja@gmail.com>
2020-08-25 23:26:56 +0300

    Improved update script

--- update.sh
+++ update.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+set -e
+UUID=2693-A1D6
 SD_DIR=/mnt/sdcard
-make && \
-sudo mount /dev/sdd1 $SD_DIR 2>/dev/zero || true && \
-ls $SD_DIR && sudo cp -fv kernel8.img $SD_DIR/u-boot.bin && sudo umount $SD_DIR
+DEV_NAME=$(sudo blkid -U $UUID)
+if make; then
+  if (cat /proc/mounts | grep $DEV_NAME); then
+    echo has mount, unmounting ...
+    sudo umount $DEV_NAME
+  fi
+  echo mounting device: $DEV_NAME uuid: $UUID
+  sudo mount --uuid $UUID $SD_DIR
+  if (ls $SD_DIR | grep 'not a '); then
+    echo mount failed
+  else
+    sudo cp -fv kernel8.img $SD_DIR/u-boot.bin
+    sudo umount $SD_DIR
+  fi
+fi
