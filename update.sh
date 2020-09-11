cacce811a2924a38b3060b2cb3647d68548d4e1f
Val <antiusninja@gmail.com>
2020-09-11 23:14:56 +0300

    some

--- update.sh
+++ update.sh
@@ -10,10 +10,11 @@ if make; then
   fi
   echo mounting device: $DEV_NAME uuid: $UUID
   sudo mount --uuid $UUID $SD_DIR
+  mount | grep sde
   if (ls $SD_DIR | grep 'not a '); then
     echo mount failed
   else
     sudo cp -fv kernel8.img $SD_DIR/u-boot.bin
-    sudo umount $SD_DIR
+    sudo umount --verbose $SD_DIR
   fi
 fi
